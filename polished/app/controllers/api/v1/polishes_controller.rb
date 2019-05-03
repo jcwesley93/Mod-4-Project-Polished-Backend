7096a598c16cd1e35460bf24858d00c264aee1bd
Jaela Wesley <github email address>
2019-05-03 16:36:13 -0400

    updted the Polish controller

--- polished/app/controllers/api/v1/polishes_controller.rb
+++ polished/app/controllers/api/v1/polishes_controller.rb
@@ -2,4 +2,44 @@ class Api::V1::PolishesController < ApplicationController
 
 #    put in the polishes controller actions
 
+    def index 
+        @polishes = Polish.all 
+        render json: @polishes
+    end
+
+    def show
+        @polish = Polish.find(params[:id])
+        render json: @polish
+    end
+    
+
+    def create
+        @polish = Polish.create(polish_params)
+        if @polish.save 
+            render json: @polish, status: :accepted
+        else
+            render json: {errors: @polish.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+
+    def update
+        @polish = Polish(params[:id])
+        @polish.update(polish_params)
+            if @polish.save
+                render json: @polish, status: :accepted
+            else
+                render json: {errors: @polish.errors.full_messages}, status: :unprocessible_entity
+            end
+    end
+
+
+
+    private
+    def polish_params(*args)
+        params.require(Polish).permit(*args)
+    end
+
+
+
 end
