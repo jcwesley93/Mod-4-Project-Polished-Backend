5b1c4461d09a199241eca01ca4fabeb4401128cd
Jaela Wesley <github email address>
2019-05-03 16:22:31 -0400

    seeded the database with initial data

--- polished/db/seeds.rb
+++ polished/db/seeds.rb
@@ -5,3 +5,96 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Polish.destroy_all
+## -- Using only enamel for now -- ##
+##Sheers
+# Polish.create!(name:, brand: "essie", color_category: "Sheers", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Sheers", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Sheers", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Sheers", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Sheers", season:, description:, price: 9.00, link:)
+
+# ##Whites 
+# Polish.create!(name:, brand: "essie", color_category: "Whites", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Whites", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Whites", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Whites", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Whites", season:, description:, price: 9.00, link:)
+
+# ##Nudes
+# Polish.create!(name:, brand: "essie", color_category: "Nudes", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Nudes", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Nudes", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Nudes", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Nudes", season:, description:, price: 9.00, link:)
+
+# ##Pinks
+# Polish.create!(name:, brand: "essie", color_category: "Pinks", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Pinks", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Pinks", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Pinks", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Pinks", season:, description:, price: 9.00, link:)
+
+##Corals 
+# Polish.create!(name:, brand: "essie",  color_category: "Reds", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie",  color_category: "Reds", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie",  color_category: "Reds", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie",  color_category: "Reds", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie",  color_category: "Reds", season:, description:, price: 9.00, link:)
+
+# ##Reds 
+# Polish.create!(name:, brand: "essie", color_category: "Reds", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Reds", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Reds", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Reds", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Reds", season:, description:, price: 9.00, link:)
+
+# ##Purples 
+# Polish.create!(name:, brand: "essie", color_category: "Purples", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Purples", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Purples", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Purples", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Purples", season:, description:, price: 9.00, link:)
+
+# ##Blues
+# Polish.create!(name:, brand: "essie", color_category: "Blues", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Blues", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Blues", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Blues", season:, description:, price: 9.00, link:)
+# Polish.create!(name:, brand: "essie", color_category: "Blues", season:, description:, price: 9.00, link:)
+
+##Greens
+Polish.create!(name: "chillato", brand: "essie", color_category: "Greens", season: "summer", description: "frozen cream pistachio", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/greens/chillato")
+Polish.create!(name: "fashion playground", brand: "essie", color_category: "Greens", season: "summer", description: "plucky pistachio shimmer", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/greens/fashion-playground")
+Polish.create!(name: "reign check", brand: "essie", color_category: "Greens", season: "winter", description: "a metallic dark teal green nail polish", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/greens/reign-check")
+Polish.create!(name: "but first, candy", brand: "essie", color_category: "Greens", season: "fall", description: "green glaze effect nail polish", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/greens/but-first-candy")
+Polish.create!(name: "mint candy apple", brand: "essie", color_category: "Greens", season: "spring", description: "a creme de menthe mint", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/greens/mint-candy-apple")
+
+##Yellows
+Polish.create!(name: "fall for nyc", brand: "essie", color_category: "Yellows", season: "summer", description: "a mustard yellow nail polish with marigold highlights (cream)", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/yellows/fall-for-nyc")
+
+##Grays
+Polish.create!(name: "press pause", brand: "essie", color_category: "Grays", season: "summer", description:"a light gray nail polish with a cloudy blue tint (cream)", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/grays/press-pause")
+
+Polish.create!(name: "cocktail bling", brand: "essie", color_category: "Grays", season: "winter", description: "a precious pale gray jewel", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/grays/cocktail-bling")
+
+Polish.create!(name: "without a stitch", brand: "essie", color_category: "Grays", season: "summer", description: "clean light gray", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/grays/without-a-stitch")
+
+Polish.create!(name: "maximillian strasse-her", brand: "essie", color_category: "Grays", season: "summer", description: "a cool grey green", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/grays/maximillian-strasse-her")
+
+Polish.create!(name: "the best-est", brand: "essie", color_category: "Grays", season: "summer", description: "a totally cool gray", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/grays/the-best-est")
+
+##Metalics & Glitters 
+Polish.create!(name: "mani thanks", brand: "essie", color_category: "Metalics & Glitters", season: "fall", description: "a shimmering champagne gold nail polish", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/metallics/mani-thanks")
+
+Polish.create!(name: "birthday girl", brand: "essie", color_category: "Metalics & Glitters", season: "spring", description: "an iridescent sheer pink nail polish (glitter)", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/metallics/birthday-girl")
+
+Polish.create!(name:"congrats", brand: "essie", color_category: "Metalics & Glitters", season: "winter", description: "a celebratory opalescent glitter. apply 2 coats for full coverage", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/metallics/congrats")
+
+Polish.create!(name: "million mile hues", brand: "essie", color_category: "Metalics & Glitters", season: "fall", description: "an illuminating gold chrome metallic nail polish", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/metallics/million-mile-hue")
+
+Polish.create!(name: "venture to the venue", brand: "essie", color_category: "Metalics & Glitters", season: "summer", description: "a shimmering crystal white crushed glutter nail polish (glitter)", price: 9.00, link: "https://www.essie.com/nail-polish/enamel/metallics/venture-to-the-venue")
+
+puts "All done!"
